c63e6050c16ac5e2ae31f7d784c961a52c83df8f
OzanAteş <ozanates22@gmail.com>
2025-07-26 07:20:00 +0300

    Add project files.

---
+++ ApiProjeKampi.WebApi/Entities/Service.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiProjeKampi.WebApi.Entities
+{
+    public class Service
+    {
+        public int ServiceID  { get; set; }
+        public int Title  { get; set; }
+        public int Description  { get; set; }
+        public int IconUrl  { get; set; }
+    }
+}
