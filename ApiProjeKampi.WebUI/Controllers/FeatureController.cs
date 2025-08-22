2009497d1c8dfbc0b4b4404404f67eb99217dbaf
OzanAteş <ozanates22@gmail.com>
2025-08-22 21:47:31 +0300

    Update

---
+++ ApiProjeKampi.WebUI/Controllers/FeatureController.cs
@@ -0,0 +1,78 @@
+﻿using ApiProjeKampi.WebUI.Dtos.FeatureDto;
+using ApiProjeKampi.WebUI.Dtos.ProductDto;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace ApiProjeKampi.WebUI.Controllers
+{
+    public class FeatureController : Controller
+    {
+        public readonly IHttpClientFactory _httpClientFactory;
+
+        public FeatureController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> FeatureList()
+        {
+            var clinet = _httpClientFactory.CreateClient();
+            var responseMessage = await clinet.GetAsync("https://localhost:7041/api/Features");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonDate = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFeatureDto>>(jsonDate);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CreateFeature()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateFeature(CreateFeatureDto createFeatureDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsondate = JsonConvert.SerializeObject(createFeatureDto);
+            StringContent stringContent = new StringContent(jsondate, Encoding.UTF8, "application/json");
+            var responseMassage = await client.PostAsync("https://localhost:7041/api/Features", stringContent);
+            if (responseMassage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("FeatureList"); 
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> DeleteFeature(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            await client.DeleteAsync("https://localhost:7041/api/Features?id=" + id); 
+
+            return RedirectToAction("FeatureList");
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateFeature(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMassage = await client.GetAsync("https://localhost:7041/api/Features/GetFeature?id=" + id);
+            var jsonDate = await responseMassage.Content.ReadAsStringAsync();
+            var value = JsonConvert.DeserializeObject<GetProductByIdDto>(jsonDate);
+            return View(value);
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateFeature(UpdateFeatureDto updateFeatureDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonDate = JsonConvert.SerializeObject(updateFeatureDto);
+            StringContent stringContent = new StringContent(jsonDate, Encoding.UTF8, "aplication/json");
+            await client.PutAsync("https://localhost:7041/api/Features", stringContent);
+            return RedirectToAction("ProductList");
+        }
+    }
+}
---
+++ ApiProjeKampi.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,95 @@
+﻿using ApiProjeKampi.WebUI.Dtos.CategoryDtos;
+using ApiProjeKampi.WebUI.Dtos.ProductDto;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace ApiProjeKampi.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+
+        }
+
+        public async Task<IActionResult> ProductList()
+        {
+            var clinet = _httpClientFactory.CreateClient();
+            var responseMessage = await clinet.GetAsync("https://localhost:7041/api/Products/ProductWhitCategory");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonDate = await responseMessage.Content.ReadAsStringAsync();
+                var vaules = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonDate);
+                return View(vaules);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CreateProduct()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7041/api/Categories");
+
+            var jsonDate = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonDate);
+            List<SelectListItem> CategoryValues = (from x in values
+                                            select new SelectListItem
+                                            {
+                                                Text= x.CategoryName,
+                                                Value= x.CategoryID.ToString()
+                                            }).ToList();
+            ViewBag.v = CategoryValues;
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(CreateProductDto createProductDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createProductDto);
+            StringContent StringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7041/api/Products/CreateProductWithCategory", StringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                //SerializeObject ve DeserializeObject ne olduğunu ve StringContent "application/json RedirectToAction bak
+
+
+                return RedirectToAction("ProductList");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> DeleteCatory(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            await client.DeleteAsync("https://localhost:7041/api/Products?id=" + id); /////////////////////////////////////////////
+
+            return RedirectToAction("ProductList");
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateProduct(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMassage = await client.GetAsync("https://localhost:7041/api/Products/GetProducts?id=" + id);
+            var jsonDate = await responseMassage.Content.ReadAsStringAsync();
+            var value = JsonConvert.DeserializeObject<GetProductByIdDto>(jsonDate);
+            return View(value);
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateProduct(UpdateProductDto updateProductDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonDate = JsonConvert.SerializeObject(updateProductDto);
+            StringContent stringContent = new StringContent(jsonDate, Encoding.UTF8, "aplication/json");
+            await client.PutAsync("https://localhost:7041/api/Products", stringContent);
+            return RedirectToAction("ProductList");
+        }
+
+
+    }
+}
